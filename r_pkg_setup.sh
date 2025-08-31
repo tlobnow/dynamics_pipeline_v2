2da77f0827c3568fda51a67a19e8bddc2cef8964
tlobnow <tlobnow@googlemail.com>
2025-08-31 21:54:47 +0200

    optimized r_pkg_setup.sh

--- r_pkg_setup.sh
+++ r_pkg_setup.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 
 Rscript -e "
-if (!requireNamespace('remotes', quietly = TRUE)) {
-    install.packages('remotes', repos='https://cloud.r-project.org')
-}
-remotes::install_version('purrr', version = '0.3.4', repos='https://cloud.r-project.org')
-remotes::install_version('tidyr', version = '1.1.3', repos='https://cloud.r-project.org')
-remotes::install_version('ijtiff', version = '2.3.1', repos='https://cloud.r-project.org')
-
 if (!requireNamespace('pacman', quietly = TRUE)) {
-    install.packages('pacman', repos='https://cloud.r-project.org')
+  install.packages('pacman', repos='https://cloud.r-project.org')
 }
+
+if (!requireNamespace('remotes', quietly = TRUE)) {
+  install.packages('remotes', repos='https://cloud.r-project.org')
+}
+
+check_and_install <- function(pkg, ver) {
+  if (!requireNamespace(pkg, quietly = TRUE) ||
+      packageVersion(pkg) != ver) {
+    remotes::install_version(pkg, version = ver,
+                             repos='https://cloud.r-project.org')
+  }
+}
+
+check_and_install('purrr', '0.3.4')
+check_and_install('tidyr', '1.1.3')
+check_and_install('ijtiff', '2.3.1')
 
 pacman::p_load(
-    XML, dplyr, parallel, tidyr, data.table, ff,
-    changepoint, compiler, stringr, dtplyr, RANN
+  XML, dplyr, parallel, tidyr, data.table, ff,
+  changepoint, compiler, stringr, dtplyr, RANN
 )
+
+message('R package setup completed successfully!')
 "
